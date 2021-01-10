264fee6db17528e234cb647bdee3c5d2d9ecd211
Zef <me@zef.sh>
2021-01-10 23:50:02 +0700

    Test callback function

--- src-backend/app.js
+++ src-backend/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Initialize modules
+dotenv.config();
 const app = express();
+const mqttClient = require('./mqtt/mqtt_client');
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
+mqttClient.onMessageReceivedCallbacks.push((topic, message) => {
+    console.log(message);
+})
